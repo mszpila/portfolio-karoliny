5a2b4238dd06f75e0f18c9fc45cb546a18a39704
Mikołaj Szpila <szpila.mikolaj@gmail.com>
2024-06-19 09:12:23 +0200

    init

---
+++ README.rd
@@ -0,0 +1,7 @@
+to produce dist folder ➜ npx tailwindcss -i ./styles.css -o ./dist/styles.css --watch
+
+to produce release ➜ zip dist, images_optimized, logos svg, index.html and tacholife png
+
+UPDATE:
+
+to produce release ➜ zip dist, assets, contactForm.js, index.html and combined-dark.png, thank-you.html, private-policy.html
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{css,html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                orange: '#FFD0AF',
+                pink:'#EF6C98',
+                white:'#F1ECE8',
+
+                background: '#FFFFFF',
+                foreground: '#F5F5F7',
+                border: '#DBDBDD',
+                visualCue: '#969699',
+                lowContrast: '#7D7D80',
+                mediumContrast: '#525254',
+                highContrast: '#212122',
+                interaction: '#1166EE',
+                error: '#CC4422',
+                completion: '#118844',
+                interactionTint: '#DDEEFF',
+                errorTint: '#FFEEEE',
+                completionTint: '#DDFFEE',
+
+                backgroundDark: '#1A1D21',
+                foregroundDark: '#282D33',
+                borderDark: '#373D44',
+                visualCueDark: '#7A8799',
+                lowContrastDark: '#6C7787',
+                mediumContrastDark: '#95A5BA',
+                highContrastDark: '#F2F4F7',
+
+                interactionDark: '#489be0',
+                errorDark: '#da0505',
+                completionDark: '#0caa48',
+                interactionTintDark: '#354d74',
+                errorTintDark: '#FFEEEE',
+                completionTintDark: '#DDFFEE',
+
+                tacholifeBLue: '#2084D8',
+            },
+        },
+    },
+    plugins: [],
+}
+
